1d01f2b5a47108f47ef2b9862a7106d60c35e1e0
Zuza <zuzannasepek@gmail.com>
2023-04-11 22:46:40 +0200

    Create match_form.py

---
+++ pages/match_form.py
@@ -0,0 +1,17 @@
+from pages.base_page import BasePage
+
+class Match_form(BasePage):
+    my_team_input_xpath = "//*[@name="myTeam"]"
+    enemy_team_input_xpath = "//*[@name="enemyTeam"]"
+    my_team_score_input_xpath = "//*[@name="myTeamScore"]"
+    enemy_score_input_xpath = "//*[@name="enemyTeamScore"]"
+    shirt_color_input_xpath = "//*[@name="tshirt"]"
+    time_played_input_xpath = "//*[@name="timePlayed"]"
+    web_match_input_xpath = "//*[@name="webMatch"]"
+    rating_input_xpath = "//*[@name="rating"]"
+    league_input_xpath = "//*[@name="league"]"
+    number_input_xpath = "//*[@name="number"]"
+    general_input_xpath = "//*[@name="general"]"
+    submit_button_xpath = "//*[@type="submit"]"
+
+    pass
