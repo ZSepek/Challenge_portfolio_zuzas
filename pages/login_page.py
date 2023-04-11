aeb7a0916a8f91869c9b95f2a982b02649d2bae5
Zuza <zuzannasepek@gmail.com>
2023-04-11 21:21:57 +0200

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,8 +5,7 @@ class LoginPage(BasePage):
     login_field_xpath = "//*[@id='login']"
     password_field_xpath = "//*[@id='password']"
     sign_in_button_xpath = "//*[@id="__next"]/form/div/div[2]/button"
-    remind_password_xpath = "//*[@id="__next"]/form/div/div[1]/a"
-    change_lang_xpath = "//*[@id="__next"]/form/div/div[2]/div/div"
+
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
